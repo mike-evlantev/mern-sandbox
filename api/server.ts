f4901597e78301c9a59f3638b3d1de3e8d4ecc8e
Mike Evlantev <mikeev21@gmail.com>
2023-03-18 13:43:22 -0700

    set up express server

--- api/server.ts
+++ api/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello World from mern-sandbox");
+import express from "express";
+import testRoutes from "./routes/testRoutes";
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/api/test', testRoutes);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+export default app;
