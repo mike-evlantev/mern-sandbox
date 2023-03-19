893ae2777166d079da859920b53a976ae9699cc9
Mike Evlantev <mikeev21@gmail.com>
2023-03-18 21:12:58 -0700

    request json body parser and error handler middleware

---
+++ api/middleware/error.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+export const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log('res', res.statusCode);
+  res
+    .status(res.statusCode || 500)
+    .json({ 
+      success: false,
+      message: error.message,
+      stack: process.env.NODE_ENV === "production" ? null : error.stack
+     });
+}
--- api/server.ts
+++ api/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { ErrorRequestHandler, RequestHandler } from "express";
+import { errorHandler } from "./middleware/error";
 import testRoutes from "./routes/testRoutes";
 
 require('dotenv').config();
@@ -6,8 +7,12 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.use('/api/test', testRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
