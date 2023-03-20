3e895ccb2f1b2885b7f99679a42e6db02ad3434a
Mike Evlantev <mikeev21@gmail.com>
2023-03-19 21:07:44 -0700

    protect order routes

--- api/routes/orders.ts
+++ api/routes/orders.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { add, getAll, get, remove, update } from "../controllers/ordersController";
+import { protect } from "../middleware/auth";
 const router = express.Router();
 
-router.get("/", getAll);
-router.get("/:id", get);
-router.post("/", add);
-router.put("/:id", update);
-router.delete("/:id", remove);
+router.get("/", protect, getAll);
+router.get("/:id", protect, get);
+router.post("/", protect, add);
+router.put("/:id", protect, update);
+router.delete("/:id", protect, remove);
 
 export default router; 
