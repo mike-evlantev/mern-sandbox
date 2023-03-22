a175ff0284c27771054914e60486fa070ff6e9db
Mike Evlantev <mikeev21@gmail.com>
2023-03-21 21:35:45 -0700

    init sign in form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,16 @@
 export const Login: React.FC = () => {
+  const handleSignIn = () => console.log('Sign In Clicked!');
   return (
-    <div>Login</div>
+    <div className="d-flex flex-column align-items-center justify-content-center h-100">
+      <div className="form-floating mb-3">
+        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+        <label>Email address</label>
+      </div>
+      <div className="form-floating mb-3">
+        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+        <label>Password</label>
+      </div>
+      <button type="submit" className="btn btn-primary" onClick={handleSignIn}>Sign In</button>
+    </div>
   )
 }
