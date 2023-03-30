f1bfec23bfdc4df1432a1def2458899fe265b1c6
Mike Evlantev <mikeev21@gmail.com>
2023-03-29 20:15:29 -0700

    toast container setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import { Login } from "./pages/Login";
 import { Orders } from "./pages/Orders";
 
@@ -14,6 +16,7 @@ function App() {
           </Routes>
         </div>        
       </Router>
+      <ToastContainer />
     </>
   );
 }
