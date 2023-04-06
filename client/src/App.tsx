171094b29416096ee5530529284a62ce47d3650d
Mike Evlantev <mikeev21@gmail.com>
2023-04-05 21:44:00 -0700

    removed test component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -28,7 +28,6 @@ function RequireAuth({ children }: { children: JSX.Element }) {
 function Layout() {
   return (
     <div>
-      <Dev>This is a DEV message</Dev>
       {/* A "layout route" is a good place to put markup you want to
           share across all the pages on your site, like navigation. */}
       <Nav />
